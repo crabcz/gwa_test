4d0c77bd14ee4b1c95ed2022d56602834947a69f
Maytyáš Rak <matyas.rak@rebelandglory.com>
2019-03-04 09:37:59 +0100

    initial commit

---
+++ create.ps1
@@ -0,0 +1,34 @@
+$resourceGroupName = "a-rg"
+$rglocation = "westeurope"
+
+New-AzResourceGroup -Name $resourceGroupName -Location $rglocation -Force
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile .\templates\commons.json `
+    -TemplateParameterFile .\$resourceGroupName\commons.json
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile .\templates\machine.json `
+    -TemplateParameterFile .\$resourceGroupName\m1.json 
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile .\templates\machine.json `
+    -TemplateParameterFile .\$resourceGroupName\m2.json 
+
+
+$resourceGroupName = "b-rg"
+$rglocation = "francecentral"
+
+New-AzResourceGroup -Name $resourceGroupName -Location $rglocation -Force
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile .\templates\commons.json ` 
+    -TemplateParameterFile .\$resourceGroupName\commons.json
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile .\templates\machine.json `
+    -TemplateParameterFile .\$resourceGroupName\m1.json 
